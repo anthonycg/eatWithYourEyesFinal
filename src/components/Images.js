3b63a6535efe1863ba026f1dbd0e083774f8c614
Anthony <anthonygibson24@gmail.com>
2022-08-02 01:00:47 -0500

    search no longer returning all results, only 1

--- src/components/Images.js
+++ src/components/Images.js
@@ -10,62 +10,79 @@ import { Link } from "react-router-dom";
 
 const Images = (props) => {
     const { photos, setPhotos} = props
-    const [modal, setModal] = useState("")
+    const [modal, setModal] = useState(false)
     
     const toggleModal = () => {
         setModal(!modal);
       };
 
-    return ( 
+    return (
         <div className="container">
-                <div className="image-gallery">
-                    {photos.map((photo, index) => {
-                        return (
-                            <button onClick={toggleModal} className="btn-modal">
-                            Open
-                          </button>
-                    
-                          {modal && (
-                            <div className="modal">
-                              <div onClick={toggleModal} className="overlay"></div>
-                              <div className="modal-content">
-                                <h2>Hello Modal</h2>
-                                <p>
-                                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
-                                  perferendis suscipit officia recusandae, eveniet quaerat assumenda
-                                  id fugit, dignissimos maxime non natus placeat illo iusto!
-                                  Sapiente dolorum id maiores dolores? Illum pariatur possimus
-                                  quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
-                                  placeat tempora vitae enim incidunt porro fuga ea.
-                                </p>
-                                <button className="close-modal" onClick={toggleModal}>
-                                  CLOSE
-                                </button>
-                              </div>
-                            </div>
-                          )}
-                            <div>
-                                
-                                <div className="image-gallery">
-                                    <Link to="/details">
-                                    <button key={index} className="image-1">
-                                        <img src={photo.image_url} 
-                                        alt="food item pic"></img>
-                                    </button>
-                                    </Link>
-                                </div>
-                                <div>
-                                    <Details currentImage={photos}/>
-                                </div>
-                            </div>
-                    ) 
+            <div className="image-gallery">
+                {photos.map((photo, index) => {
+                    return (modal === false ?
+                    <button onClick={toggleModal} key={index} className="image-1">
+                    <img src={photo.image_url} 
+                    alt="food item pic"></img>
+                    </button>:
+                    <div className="modal">
+                        <div onClick={toggleModal} className="overlay"></div>
+                            <div className="modal-content">
+                                <h2>{photo.name}</h2>
+                                    <p> {photo.name}
+                                    </p>
+                    <button className="close-modal" onClick={toggleModal}>
+                        CLOSE
+                    </button>
+                    </div>
+                    </div>
+                            )
+                        }
+                    )
                 }
-            )
-        }
-                </div>
-                {/* <input type="submit" /> */}
+            </div>
         </div>
     )
 }
+
+
+
+        // <div className="container">
+        //         <div className="image-gallery">
+        //             {photos.map((photo, index) => {
+        //                 return (
+                        //     <button onClick={toggleModal} className="btn-modal">
+                        //     Open
+                        //   </button>
+                    
+                        //   {if (modal) {
+                        //     <div className="modal">
+                        //       <div onClick={toggleModal} className="overlay"></div>
+                        //       <div className="modal-content">
+                        //         <h2>Hello Modal</h2>
+                        //         <p>
+                        //           Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident
+                        //           perferendis suscipit officia recusandae, eveniet quaerat assumenda
+                        //           id fugit, dignissimos maxime non natus placeat illo iusto!
+                        //           Sapiente dolorum id maiores dolores? Illum pariatur possimus
+                        //           quaerat ipsum quos molestiae rem aspernatur dicta tenetur. Sunt
+                        //           placeat tempora vitae enim incidunt porro fuga ea.
+                        //         </p>
+                        //         <button className="close-modal" onClick={toggleModal}>
+                        //           CLOSE
+                        //         </button>
+                        //       </div>
+                        //     </div>
+                        //   } else {
+                        //       <div className="image-gallery">
+                        //           {/* <Link to="/details"> */}
+                        //           <button onClick={toggleModal} key={index} className="image-1">
+                        //               <img src={photo.image_url} 
+                        //               alt="food item pic"></img>
+                        //           </button>
+                        //           {/* </Link> */}
+                        //       </div>
+                        //   }})})}
+
 
 export default Images;
