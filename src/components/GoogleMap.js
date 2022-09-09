5d135d43fd3cf51ad50992b0480d5427bc27b4d2
Anthony <anthonygibson24@gmail.com>
2022-09-08 23:56:52 -0500

    testing new way to render google map

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -43,36 +43,68 @@
 
 
 import React from 'react'
-import { GoogleMap, LoadScript } from '@react-google-maps/api';
+import { useRef, useState, useEffect } from 'react';
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+// import { GoogleMap, LoadScript } from '@react-google-maps/api';
 
 
   const containerStyle = {
-    width: '30rem',
-    height: '30rem'
-  };
-  
-  
-  const GoogleMapComponent = (props) => {
-    const {lat, long} = props
-    console.log(lat, long)
-    let center = {
-      lat: lat,
-      lng: long
+      width: '30rem',
+      height: '30rem'
     };
-    return (
-      <LoadScript
-        googleMapsApiKey = {process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
-      >
-        <GoogleMap
-          mapContainerStyle={containerStyle}
-          center={center}
-          zoom={15}
-        >
-          { /* Child components, such as markers, info windows, etc. */ }
-          <></>
-        </GoogleMap>
-      </LoadScript>
-    )
-  }
+  
+  
+  //   const GoogleMapComponent = (props) => {
+        // const {lat, long} = props
+    //     console.log(lat, long)
+        // let center = {
+        //     lat: lat,
+        //     lng: long
+        //   };
+      //     return (
+        //       <LoadScript
+        //         googleMapsApiKey = {process.env.REACT_APP_GOOGLE_MAPS_API_KEY}
+        //       >
+        //         <GoogleMap
+        //           mapContainerStyle={containerStyle}
+        //           center={center}
+        //           zoom={15}
+        //         >
+        //           { /* Child components, such as markers, info windows, etc. */ }
+        //           <></>
+        //         </GoogleMap>
+        //       </LoadScript>
+        //     )
+        //   }
+        
+        // export default GoogleMapComponent;
+        
+const render = (status = Status) => {
+  const Map = React.FC = () => {};
+  return <h1>{status}</h1>;
+};
+const GoogleMapComponent = (props) => {
+  const {lat, long} = props
+  let center = {
+    lat: lat,
+    lng: long
+  };
+  const ref = useRef(null);
+  const [map, setMap] = useState();
+  
+  useEffect(() => {
+    if (ref.current && !map) {
+      setMap(window.google.maps.Map(ref.current, {}));
+    }
+  }, [ref, map]);
+  return <div ref={ref}>
+    <Wrapper apiKey={process.env.REACT_APP_GOOGLE_MAPS_API_KEY} render={render}>
+      <GoogleMapComponent center = {center} zoom = {15}/>
+    </Wrapper>
+  </div>
+}
+
 
 export default GoogleMapComponent;
+
+
